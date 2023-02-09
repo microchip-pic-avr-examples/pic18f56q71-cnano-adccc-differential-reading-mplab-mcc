94948b1ba43f2060ccc0fda24a072b97af61745b
M17319 <marius.nicolae@microchip.com>
2023-02-09 14:33:49 -0700

    MPAE-15455 remove more artifacts from main

--- Q71_diff_reading.X/main.c
+++ Q71_diff_reading.X/main.c
@@ -42,9 +42,7 @@ int main(void)
 {
     SYSTEM_Initialize();
     
-    int8_t resultHigh, resultLow;
     int16_t result;
-    float conversion;
 
     // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts 
     // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global Interrupts 
